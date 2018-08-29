f1037038a963e98e055db0b22d5acefcf57d8f2d
masahiro_kanatani <masahiro_kanatani@kronos-jp.net>
2018-08-29 20:58:45 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-  return (
-    <React.Fragment>
-      <label htmlFor="bar">Bar</label>
-        <input type="text" onChange={() => {console.log("Hi!")}} />
-    </React.Fragment>
-  
-  )
-  }
+const App = () => {
+    const profiles = [
+        { name : "Taro", age : 10},
+        { name : "Hanako", age : 5},
+        { name : "None"}
+    ]
+    return (
+        <div>
+            {
+            profiles.map((profile, index) => {
+            return <User name={profile.name} age={profile.age} key={index}/>
+            })
+            }
+        </div>
+    )
+}
+
+const User = (props) => {
+    return <div>I am {props.name}! And I am {props.age} years old ! </div>
+}
+
+User.defaultProps = {
+    age : 1
 }
 
 export default App;
